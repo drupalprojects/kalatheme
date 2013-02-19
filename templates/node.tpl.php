1464039f3b4bc4ae3c019d859dd561a5d84aabf9
Mike Pirog <mike@kalamuna.com>
2013-02-19 08:55:03 -0800

    theoretically all nodes are panelizers

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -105,11 +105,6 @@
   </div>
 
   <?php
-    // Remove the "Add new comment" link on the teaser page or if the comment
-    // form is being displayed on the same page.
-    if ($teaser || !empty($content['comments']['comment_form'])) {
-      unset($content['links']['comment']['#links']['comment-add']);
-    }
     // Only display the wrapper div if there are links.
     $links = render($content['links']);
     if ($links):
