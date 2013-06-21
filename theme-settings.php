84cd1889f703b52b2372b879a6fd51bbde325edc
Mike Pirog <mike@kalamuna.com>
2013-06-21 01:18:20 +0000

    Issue #2008634 by pirog: Kalacustomize for panels layout regions

--- theme-settings.php
+++ theme-settings.php
@@ -19,13 +19,13 @@ function kalatheme_form_system_theme_settings_alter(&$form, &$form_state) {
     '#weight' => 42,
     '#collapsible' => FALSE,
     '#collapsed' => FALSE,
-    '#description' => t('If toggled on, the kalacustomize style plugin will allow the user to configure pane device visibilty.'),
+    '#description' => t('If toggled on, the kalacustomize style plugin will allow the user to configure device visibilty for regions and panes.'),
   );
   $form['responsive']['responsive_toggle'] = array(
     '#type' => 'checkbox',
     '#title' => t('Use responsive toggling.'),
     '#default_value' => theme_get_setting('responsive_toggle'),
-    '#description' => t('Check here if you want the user to be able to set the responsive visbility of each panels pane.'),
+    '#description' => t('Check here if you want the user to be able to set the responsive visbility of each panels pane and region.'),
   );
 
   // Panels styles style plugin settings.
@@ -68,7 +68,7 @@ function kalatheme_form_system_theme_settings_alter(&$form, &$form_state) {
   // Extra styles style plugin settings.
   $form['extra_styles'] = array(
     '#type' => 'fieldset',
-    '#title' => t('Extra Styles'),
+    '#title' => t('Extra Pane Styles'),
     '#weight' => 44,
     '#collapsible' => FALSE,
     '#collapsed' => FALSE,
@@ -76,7 +76,7 @@ function kalatheme_form_system_theme_settings_alter(&$form, &$form_state) {
   );
   $form['extra_styles']['extra_styles_toggle'] = array(
     '#type' => 'checkbox',
-    '#title' => t('Use extra styles.'),
+    '#title' => t('Use extra pane styles.'),
     '#default_value' => theme_get_setting('extra_styles_toggle'),
     '#description' => t('Check here if you want to customize the elements and classes for pane titles and content.'),
   );
@@ -107,6 +107,43 @@ function kalatheme_form_system_theme_settings_alter(&$form, &$form_state) {
     '#type' => 'textarea',
     '#title' => t('Allowed values list'),
     '#default_value' => $extra_classes,
+    '#rows' => 10,
+    '#element_validate' => array('list_allowed_values_setting_validate'),
+    '#field_has_data' => FALSE,
+    '#field' => $field,
+    '#field_type' => $field['type'],
+    '#description' => '<p>' . t('The possible values this field can contain. Enter one value per line, in the format key|label.'),
+  );
+  
+  // Region styles style plugin settings.
+  $form['region_styles'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Region Styles'),
+    '#weight' => 55,
+    '#collapsible' => FALSE,
+    '#collapsed' => FALSE,
+    '#description' => t('If toggled on, the kalacustomize style plugin will allow the user to set a class for panels regions.'),
+  );
+  $form['region_styles']['region_styles_toggle'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Use region styles.'),
+    '#default_value' => theme_get_setting('region_styles_toggle'),
+    '#description' => t('Check here if you want to set the class for each panels region.'),
+  );
+  $form['region_styles']['region_styles_settings'] = array(
+    '#type' => 'container',
+    '#states' => array(
+      'invisible' => array(
+        'input[name="region_styles_toggle"]' => array('checked' => FALSE),
+      ),
+    ),
+  );
+  // Set defaults here instead of info because it is an array.
+  $region_classes = (theme_get_setting('region_classes')) ? list_allowed_values_string(theme_get_setting('region_classes')) : '';
+  $form['region_styles']['region_styles_settings']['region_classes'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Allowed values list'),
+    '#default_value' => $region_classes,
     '#rows' => 10,
     '#element_validate' => array('list_allowed_values_setting_validate'),
     '#field_has_data' => FALSE,
