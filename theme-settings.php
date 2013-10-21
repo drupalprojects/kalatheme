02a74ec39f84af5223557a54cbeb20b6e873c388
Mike Pirog <mike@kalamuna.com>
2013-10-21 21:16:12 +0000

    Issue #2055609 by vulfox: Make styles additive, simplify style options

--- theme-settings.php
+++ theme-settings.php
@@ -59,17 +59,17 @@ function kalatheme_form_system_theme_settings_alter(&$form, &$form_state) {
   // Panels styles style plugin settings.
   $form['pane_styles'] = array(
     '#type' => 'fieldset',
-    '#title' => t('Pane Styles'),
+    '#title' => t('Pane and Region Styles'),
     '#weight' => 43,
     '#collapsible' => FALSE,
     '#collapsed' => FALSE,
-    '#description' => t('If toggled on, the kalacustomize style plugin will allow the user to set a class for panels panes.'),
+    '#description' => t('If toggled on, the kalacustomize style plugin will allow the user to set a class for panels panes and regions.'),
   );
   $form['pane_styles']['pane_styles_toggle'] = array(
     '#type' => 'checkbox',
     '#title' => t('Use panels styles.'),
     '#default_value' => theme_get_setting('pane_styles_toggle'),
-    '#description' => t('Check here if you want to set the class for each panels pane.'),
+    '#description' => t('Check here if you want to set the class for each panels pane or region.'),
   );
   $form['pane_styles']['pane_styles_settings'] = array(
     '#type' => 'container',
@@ -93,90 +93,4 @@ function kalatheme_form_system_theme_settings_alter(&$form, &$form_state) {
     '#description' => '<p>' . t('The possible values this field can contain. Enter one value per line, in the format key|label.'),
   );
 
-  // Extra styles style plugin settings.
-  $form['extra_styles'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Extra Pane Styles'),
-    '#weight' => 44,
-    '#collapsible' => FALSE,
-    '#collapsed' => FALSE,
-    '#description' => t('If toggled on, the kalacustomize style plugin will allow the user to set elements and classes for panels panes titles and content.'),
-  );
-  $form['extra_styles']['extra_styles_toggle'] = array(
-    '#type' => 'checkbox',
-    '#title' => t('Use extra pane styles.'),
-    '#default_value' => theme_get_setting('extra_styles_toggle'),
-    '#description' => t('Check here if you want to customize the elements and classes for pane titles and content.'),
-  );
-  $form['extra_styles']['extra_styles_settings'] = array(
-    '#type' => 'container',
-    '#states' => array(
-      'invisible' => array(
-        'input[name="extra_styles_toggle"]' => array('checked' => FALSE),
-      ),
-    ),
-  );
-  // Set defaults here instead of info because it is an array.
-  $extra_elements = (theme_get_setting('extra_elements')) ? list_allowed_values_string(theme_get_setting('extra_elements')) : '';
-  $form['extra_styles']['extra_styles_settings']['extra_elements'] = array(
-    '#type' => 'textarea',
-    '#title' => t('Allowed elements'),
-    '#default_value' => $extra_elements,
-    '#rows' => 10,
-    '#element_validate' => array('list_allowed_values_setting_validate'),
-    '#field_has_data' => FALSE,
-    '#field' => $field,
-    '#field_type' => $field['type'],
-    '#description' => '<p>' . t('The possible values this field can contain. Enter one value per line, in the format key|label.'),
-  );
-  // Set defaults here instead of info because it is an array.
-  $extra_classes = (theme_get_setting('extra_classes')) ? list_allowed_values_string(theme_get_setting('extra_classes')) : '';
-  $form['extra_styles']['extra_styles_settings']['extra_classes'] = array(
-    '#type' => 'textarea',
-    '#title' => t('Allowed classes'),
-    '#default_value' => $extra_classes,
-    '#rows' => 10,
-    '#element_validate' => array('list_allowed_values_setting_validate'),
-    '#field_has_data' => FALSE,
-    '#field' => $field,
-    '#field_type' => $field['type'],
-    '#description' => '<p>' . t('The possible values this field can contain. Enter one value per line, in the format key|label.'),
-  );
-
-  // Region styles style plugin settings.
-  $form['region_styles'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Region Styles'),
-    '#weight' => 55,
-    '#collapsible' => FALSE,
-    '#collapsed' => FALSE,
-    '#description' => t('If toggled on, the kalacustomize style plugin will allow the user to set a class for panels regions.'),
-  );
-  $form['region_styles']['region_styles_toggle'] = array(
-    '#type' => 'checkbox',
-    '#title' => t('Use region styles.'),
-    '#default_value' => theme_get_setting('region_styles_toggle'),
-    '#description' => t('Check here if you want to set the class for each panels region.'),
-  );
-  $form['region_styles']['region_styles_settings'] = array(
-    '#type' => 'container',
-    '#states' => array(
-      'invisible' => array(
-        'input[name="region_styles_toggle"]' => array('checked' => FALSE),
-      ),
-    ),
-  );
-  // Set defaults here instead of info because it is an array.
-  $region_classes = (theme_get_setting('region_classes')) ? list_allowed_values_string(theme_get_setting('region_classes')) : '';
-  $form['region_styles']['region_styles_settings']['region_classes'] = array(
-    '#type' => 'textarea',
-    '#title' => t('Allowed values list'),
-    '#default_value' => $region_classes,
-    '#rows' => 10,
-    '#element_validate' => array('list_allowed_values_setting_validate'),
-    '#field_has_data' => FALSE,
-    '#field' => $field,
-    '#field_type' => $field['type'],
-    '#description' => '<p>' . t('The possible values this field can contain. Enter one value per line, in the format key|label.'),
-  );
 }
